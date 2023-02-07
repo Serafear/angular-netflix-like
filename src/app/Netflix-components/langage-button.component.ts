928e4c2b35872c6ec333e705c8c8eadf23029e07
Serafear <72019501+Serafear@users.noreply.github.com>
2023-02-07 14:13:17 +0100

    1 binaries uploaded

---
+++ src/app/Netflix-components/langage-button.component.ts
@@ -0,0 +1,21 @@
+import 'zone.js/dist/zone';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-langbtn',
+  templateUrl: './langage-button.component.html',
+  styleUrls: ['./langage-button.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class LangageButtonComponent {
+  isOpen = false;
+  selectedLanguage = 'English';
+  languages = ['English', 'French', 'German', 'Spanish'];
+
+  selectLanguage(language: string) {
+    this.selectedLanguage = language;
+    this.isOpen = false;
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {NetflixComponent} from './Netflix/Netflix.component'
+import { NetflixComponent } from './Netflix/Netflix.component';
+import { CommonModule } from '@angular/common';
+import { LangageButtonComponent } from './Netflix-components/langage-button.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterModule, NetflixComponent],
+  imports: [RouterModule, NetflixComponent, CommonModule, LangageButtonComponent],
 })
 export class AppComponent {
   title = 'angular-standalone-components-example';
+
 }
