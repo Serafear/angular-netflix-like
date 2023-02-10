1327ce8d50640a2e5a69bf784e63aab8f0469bb4
Serafear <72019501+Serafear@users.noreply.github.com>
2023-02-10 23:51:30 +0100

    3 files modified

--- src/app/Netflix-landing/FAQ/faq.component.ts
+++ src/app/Netflix-landing/FAQ/faq.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,28 +7,42 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./faq.component.css'],
   standalone: true,
   imports: [CommonModule],
+  encapsulation:
+    ViewEncapsulation.None /*this allow me to use the class underline below*/,
 })
-
 export class FaqComponent {
   faqs = [
     {
-      question: 'What is Angular?',
-      answer: 'Angular is a JavaScript framework for building web applications. It is maintained by Google and is used for building dynamic, single-page applications.',
-      isOpen: false
+      question: "Netflix, qu'est ce que c'est?",
+      answer:
+        "Netflix est un service de streaming qui propose une vaste sélection de séries, films, animes, documentaires et autres programmes sur des milliers d'appareils connectés à Internet. ",
+      answerSecond:
+        'Regardez tout ce que vous voulez, quand vous voulez, à un prix mensuel très attractif. Découvrez de nouveaux films et séries chaque semaine, il y en a pour tous les goûts !',
+      isOpen: false,
     },
     {
-      question: 'What are the benefits of using Angular?',
-      answer: 'There are many benefits to using Angular, including its ability to simplify the development process, improve code quality, and increase development speed. Additionally, Angular has a large and active community, making it easy to find support and resources when needed.',
-      isOpen: false
-    }
+      question: 'Combien coûte Netflix ?',
+      answer:
+        'Regardez Netflix sur votre smartphone, tablette, Smart TV, ordinateur ou appareil de streaming, le tout pour un tarif mensuel fixe. Les offres vont de 5,99 € à 17,99 € par mois. Pas de contrat ni de frais supplémentaires. ',
+      answerSecond: '',
+      isOpen: false,
+    },
+    {
+      question: "Qu'est ce qui est différent dans Essentiel avec pub ?",
+      answer:
+        "Essentiel avec pub vous permet de profiter de films et séries à un prix plus attractif. Vous pouvez regarder vos programmes préférés sur n'importe quel appareil avec de courtes pauses pub. Cette offre ne donne pas accès aux téléchargements, et quelques films et séries ne sont pas disponibles en raison de restrictions liées aux licences. Certaines restrictions sur l'emplacement et les appareils s'appliquent également. <span class='underline'>En savoir plus.</span> ",
+      answerSecond: '',
+      isOpen: false,
+    },
   ];
 
-  toggleFAQ(faq) {
-    faq.isOpen = !faq.isOpen;
+  toggleFAQ(faqToOpen) {
+    this.faqs.forEach((faq) => {
+      if (faq === faqToOpen) {
+        faq.isOpen = !faq.isOpen;
+      } else {
+        faq.isOpen = false;
+      }
+    });
   }
 }
-
-
-
-
-
