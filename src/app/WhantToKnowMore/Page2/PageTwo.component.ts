2eafb683d30421a3ecad26edf535eba6477f6ef2
Serafear <72019501+Serafear@users.noreply.github.com>
2023-02-17 22:48:20 +0100

    2 files modified

--- src/app/WhantToKnowMore/Page2/PageTwo.component.ts
+++ src/app/WhantToKnowMore/Page2/PageTwo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { WtkmFooterComponent } from '../Wtkm-Footer/footer.component';
 import { TopBandComponent } from '../Wtkm-topBand/top-band.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -9,10 +10,16 @@ import { TopBandComponent } from '../Wtkm-topBand/top-band.component';
   templateUrl: './PageTwo.component.html',
   styleUrls: ['./PageTwo.component.css'],
   standalone: true,
-  imports: [RouterModule, WtkmFooterComponent, TopBandComponent],
+  imports: [RouterModule, WtkmFooterComponent, TopBandComponent, CommonModule],
 })
 export class PageTwoComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'];
+  showFourthColumn = false;
+
+  toggleFourthColumn() {
+    this.showFourthColumn = !this.showFourthColumn;
+  }
 }
