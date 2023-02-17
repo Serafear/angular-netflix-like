cb56a9c18ebdc668d906807ab66c1d9e9503ff74
Serafear <72019501+Serafear@users.noreply.github.com>
2023-02-17 14:16:37 +0100

    6 files modified

--- src/app/WhantToKnowMore/Page1/pageOne.component.ts
+++ src/app/WhantToKnowMore/Page1/pageOne.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { WtkmFooterComponent } from '../Wtkm-Footer/footer.component'
 
 @Component({
   selector: 'app-pageOne',
   templateUrl: './pageOne.component.html',
   styleUrls: ['./pageOne.component.css'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, WtkmFooterComponent],
 })
 export class PageOneComponent implements OnInit {
   constructor() {}
--- src/app/WhantToKnowMore/Wtkm-Footer/footer.component.ts
+++ src/app/WhantToKnowMore/Wtkm-Footer/footer.component.ts
@@ -1,16 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { LangageButtonComponent } from '../../Netflix-components/langage-button.component'
-
+import { CommonModule } from '@angular/common';
+import { LangageButtonComponent } from '../../Netflix-components/langage-button.component';
 
 @Component({
   selector: 'app-wtkm-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
   standalone: true,
-  imports: [LangageButtonComponent ],
-  encapsulation:
-    ViewEncapsulation.None /*this allow me to use ng-container*/,
+  imports: [LangageButtonComponent, CommonModule],
+  encapsulation: ViewEncapsulation.None /*this allow me to use ng-container*/,
 })
-export class WtkmFooterComponent {
-
-}
+export class WtkmFooterComponent {}
