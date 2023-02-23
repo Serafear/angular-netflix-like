240f8d45bf15670a1b5593b963a341e75e2da67c
Serafear <72019501+Serafear@users.noreply.github.com>
2023-02-23 22:16:28 +0100

    2 files modified

--- src/app/WhantToKnowMore/Page3/PageThree.component.ts
+++ src/app/WhantToKnowMore/Page3/PageThree.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { WtkmFooterComponent } from '../Wtkm-Footer/footer.component';
 import { TopBandComponent } from '../Wtkm-topBand/top-band.component';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-pageThree',
@@ -9,9 +10,12 @@ import { TopBandComponent } from '../Wtkm-topBand/top-band.component';
   styleUrls: ['./PageThree.component.css'],
   standalone: true,
   imports: [RouterModule, WtkmFooterComponent, TopBandComponent],
+
 })
 export class PageThreeComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+
 }
